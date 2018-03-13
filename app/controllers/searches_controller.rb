fbec78a67f727a8b6c0bbbed59b81d3d51fe637c
smithwebtek <brad@smithwebtek.com>
2018-03-13 09:32:45 -0400

    Cloudinary action not working

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -13,29 +13,29 @@ class SearchesController < ApplicationController
 		# puts "the URL: " + "#{url}"
 		# puts "--------------------------------------"
 		
-		url = 'https://' + "#{ENV["API_KEY"]}:" + "#{ENV["API_SECRET"]}" + '@api.cloudinary.com/v1_1/smithwebtek/resources/search'
-
-	def foursquare
+		
+	def cloudinary
 		begin
-			# @resp = Faraday.get 'https://api.cloudinary.com/v1_1/smithwebtek/resources/image' do |req|
-			@resp = Faraday.get url do |req|
-				req.params["cloud_name"] = 'smithwebtek'
-				req.params["api_key"] = ENV["API_KEY"]
-				req.params["api_secret"] = ENV["API_SECRET"]
-			end
-			body = JSON.parse(@resp.body) 
-			binding.pry
+		# @resp = Faraday.get 'https://api.cloudinary.com/v1_1/smithwebtek/resources/image' do |req|
+		url = 'https://' + "#{ENV["API_KEY"]}:" + "#{ENV["API_SECRET"]}" + '@api.cloudinary.com/v1_1/smithwebtek/resources/search'
+		@resp = Faraday.get '' do |req|
+			req.params["cloud_name"] = 'smithwebtek'
+			req.params["api_key"] = ENV["API_KEY"]
+			req.params["api_secret"] = ENV["API_SECRET"]
 		end
+		body = JSON.parse(@resp.body) 
+		binding.pry
 		
 		if @resp.success?
 			@images = body["response"]["images"]
 		else
 			@error = body
 		end
-		
+	
 		rescue Faraday::TimeoutError
 			@error = "There was a timeout. Please try again."
 		end
 		render 'search'
+		end
 	end
 end
